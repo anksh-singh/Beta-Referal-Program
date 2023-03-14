dede39d1ce22e445eb194ccc91b6679f0cdad60f
ankitsingh1999 <ankitsi031999@gmail.com>
2023-03-14 13:28:11 +0530

    open google sheet with sheet id

--- referral/views.py
+++ referral/views.py
@@ -10,12 +10,12 @@ def send_email(request):
     #fetch all the white listed users
     # set up authentication
     scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('path/to/credentials.json', scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
     client = gspread.authorize(creds)
-
-    # # open the Google Sheet using sheet ID
-    # sheet_id = 'your_sheet_id_here'
-    # sheet = client.open_by_key(sheet_id).sheet1
+    # open the Google Sheet using sheet ID
+    sheet_id = '1G6PzAEXCBIPxNSF12f1pw05IyS4bl90jocX3xpE3Pjc'
+    sheet = client.open_by_key(sheet_id).sheet1
+    print(sheet)
 
     # # read the data from the sheet
     # data = sheet.get_all_values()
