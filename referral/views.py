cee31b572e2f0360bc36c0089db338ebfe665df6
ankitsingh1999 <ankitsi031999@gmail.com>
2023-03-14 23:27:12 +0530

    open google sheet with sheet id

--- referral/views.py
+++ referral/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render, HttpResponse
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import os, json
 
 
 
 # Create your views here.
 
 def send_email(request):
-    #fetch all the white listed users
-    # set up authentication
-    scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-    client = gspread.authorize(creds)
-    # open the Google Sheet using sheet ID
-    sheet_id = '1G6PzAEXCBIPxNSF12f1pw05IyS4bl90jocX3xpE3Pjc'
-    sheet = client.open_by_key(sheet_id).sheet1
-    print(sheet)
-
-    # # read the data from the sheet
-    # data = sheet.get_all_values()
-    # print(data)
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    #read credentials from file
+    with open('credentials.json', 'r') as f:
+        data_= json.load(f)
+        print("cred_data", type(data_))
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(data_, scope)
+        # authenticate using the credentials
+        client = gspread.authorize(creds)
+        open the Google Sheet by its ID
+        sheet_id = 'your_sheet_id_here'
+        sheet = client.open_by_key(sheet_id).sheet1
+        # get all the data from the sheet
+        data = sheet.get_all_records()
+        print(creds)
 
     return HttpResponse("ok")
