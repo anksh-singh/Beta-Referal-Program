948fb3c2651d843ef9c53f598a306b0a74da3840
ankitsingh1999 <ankitsi031999@gmail.com>
2023-03-28 17:34:58 +0530

    user authorities validation view func

--- referral/views.py
+++ referral/views.py
@@ -24,3 +24,40 @@ def send_email(request):
         print(creds)
 
     return HttpResponse("ok")
+
+
+
+@api_view(['GET'])
+def validate_user_authorities(request):
+    response, data = {}, {}
+    email = request.GET.get('email',None)
+    location = request.GET.get('location', None)
+    if not email:                                                             
+        response['status_code'], response['message'] = 400, 'email is required'
+        return JsonResponse(response)
+    sheet = get_client()
+    if not sheet:
+        response['status_code'], response['message'] = 500, 'Internal error'
+        return JsonResponse(response)
+    
+    all_values = sheet.get_all_records()
+    full_row = check_value_exists('Email', email, all_values)
+    if full_row:
+        if full_row['is_whitelisted'] == 'TRUE':
+            utc_now = datetime.datetime.now(datetime.timezone.utc)
+            email_cell_row = get_index('Email', email, all_values)
+            if email_cell_row != None:
+                email_cell_row += 2
+            sheet.update_cell(email_cell_row, 9, utc_now.strftime('%Y-%m-%d %H:%M:%S'))
+            if location:
+                sheet.update_cell(email_cell_row, 8, location)
+            data['is_whitelisted'] = True
+        else:
+            data['is_whitelisted'] = False
+        response['data'],response['status_code']=data, 200
+        response['message'] = 'User is whitelisted'
+        return JsonResponse(response)
+    else:
+        data['is_whitelisted'] = False
+        response['data'],response['status_code'], response['message']=data, 404, 'User does not exist'
+        return JsonResponse(response)
